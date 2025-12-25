32dc7701822ecbd8464df17ab656bb2e3538715a
Vijay <vijayrajeshr@gmail.com>
2025-12-25 21:50:51 +0530

    feature 1 : hav added conditional statements

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 n = int(input('\n\tEnter val of N :'))
 
 def iteration_ink(n):
-
-    for i in range(n+1):
-        print(f'{i}th Iteration ---')
+    
+    if n<100:
+        for i in range(n+1):
+            print(f'{i}th Iteration ---')
+    else:
+        print(f'what do you mean by {n}. Im too lazy to print!')
     
 iteration_ink(n)  
+
